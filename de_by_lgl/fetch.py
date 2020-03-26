8a7651895a90f5b61b5eb785342942ae3b6b3829
Stephan Hohe <sth.dev@tejp.de>
2020-03-27 00:33:09 +0100

    Parse new table with deaths

--- de_by_lgl/fetch.py
+++ de_by_lgl/fetch.py
@@ -10,7 +10,7 @@ ap = argparse.ArgumentParser()
 fetchhelper.add_arguments(ap)
 args = ap.parse_args()
 
-import subprocess, datetime, re, csv, os
+import subprocess, datetime, re, csv, os, sys
 from bs4 import BeautifulSoup
 import dateutil.tz
 
@@ -23,37 +23,75 @@ if args.only_changed:
         print("downloaded raw data unchanged")
         exit(0)
 
+# accidentally duplicated <tr>
+update.rawdata = re.sub(r'<tr>\s*<tr>', '<tr>', update.rawdata)
 html = BeautifulSoup(update.rawdata, 'html.parser')
+
+def get_labeltime(text):
+    mo = re.search(r'Stand:? (\S* \S*)', text)
+    if mo is None:
+        return None
+    try:
+        return datetime.datetime.strptime(mo.group(1), '%d.%m.%Y %H:%M') \
+            .replace(tzinfo=datatz)
+    except ValueError:
+        return datetime.datetime.strptime(mo.group(1), '%d.%m.%Y, %H:%M') \
+            .replace(tzinfo=datatz)
 
 for p in html.select('.bildunterschrift'):
     text = p.get_text()
-    mo = re.search('Stand: (\S* \S*)', text)
-    if mo is None:
-        continue
-    update.contenttime = datetime.datetime.strptime(mo.group(1), '%d.%m.%Y %H:%M') \
-        .replace(tzinfo=datatz)
-    break
+    update.contenttime = get_labeltime(text)
+    if update.contenttime is not None:
+        break
 
 if update.contenttime is None:
-    print("Couldn't find content time")
+    print("Couldn't find content time", file=sys.stderr)
     exit(1)
 
+def tab_rows(tab):
+    trs = tab.select('tr')
+    assert('kreis' in trs[0].find('th').get_text() or 'bezirk' in trs[0].find('th').get_text())
+    assert('Gesamt' in trs[-1].find('td').get_text())
+    trs = trs[1:-1]
 
-def parse_table(parse, html, tabselect):
+    rows = []
+    for tr in trs:
+        tds = tr.select('td')
+        assert(len(tds) == 2)
+        rows.append((tds[0].get_text(), int(tds[1].get_text())))
+    return rows
+
+def parse_table(parse, html, select, *, optional=False):
+    tables = []
     for tab in html.select('table'):
-        if tabselect(tab):
-            with open(parse.parsedfile, 'w') as outf:
-                cout = csv.writer(outf)
-                cout.writerow(['Area', 'Date', 'Confirmed'])
-                for tr in tab.select('tr')[1:-1]:
-                    tds = tr.select('td')
-                    assert(len(tds) == 2)
-                    cout.writerow([tds[0].string, parse.parsedtime.isoformat(), tds[1].string])
-            parse.diff()
-            break
-    else:
-        print("couldn't find table %s" % parse.label)
+        if select(tab):
+            tables.append(tab)
+
+    if not tables:
+        if optional:
+            return
+        print("couldn't find table %s" % parse.label, file=sys.stderr)
         exit(1)
+
+    if len(tables) > 1:
+        print("found multiple tables %s" % parse.label, file=sys.stderr)
+        exit(1)
+
+    caption = tables[0].find('caption')
+    if caption is not None:
+        datatime = get_labeltime(caption.get_text())
+    else:
+        datatime = parse.parsedtime
+    if datatime is None:
+        print("couldn't determine datatime for %s" % parse.label, file=sys.stderr)
+        exit(1)
+
+    with open(parse.parsedfile, 'w') as outf:
+        cout = csv.writer(outf)
+        cout.writerow(['Area', 'Date', 'Deaths' if 'deaths' in parse.label else 'Confirmed'])
+        for tds in tab_rows(tables[0]):
+            cout.writerow([tds[0], datatime.isoformat(), tds[1]])
+    parse.diff()
 
     if args.only_changed:
         if not parse.parseddiff.changed:
@@ -63,19 +101,38 @@ def parse_table(parse, html, tabselect):
     # It changed. If the current day is later than the contenttime we assume the
     # content time is a mistake and we adjust it to the current day.
     # (This problem has happend before)
-    if parse.update.rawtime.date() > parse.parsedtime.date():
-        if parse.parseddiff.changed and not parse.parseddiff.first:
-            print("Adjust date", parse.parsedtime, "->", parse.update.rawtime)
-            parse.parsedtime = parse.update.rawtime
+    #if parse.update.rawtime.date() > parse.parsedtime.date():
+    #    if parse.parseddiff.changed and not parse.parseddiff.first:
+    #        print("Adjust date", parse.parsedtime, "->", parse.update.rawtime)
+    #        parse.parsedtime = parse.update.rawtime
 
     parse.deploy_day()
     print("written %s" % parse.deployfile)
 
-rparse = fetchhelper.ParseData(update, 'regierungsbezirk')
-parse_table(rparse, html,
-        (lambda tab: tab.select_one('th').string == 'Regierungsbezirk'))
-lparse = fetchhelper.ParseData(update, 'landkreis')
-parse_table(lparse, html,
-        (lambda tab: tab.select_one('th').string in ['Landkreis', 'Land-/Stadtkreis', 'Landkreis/Stadt']))
+def is_regierungsbezirk(tab):
+    return tab.select_one('th').string in ['Regierungsbezirk']
 
-fetchhelper.git_commit([rparse, lparse], args)
+def is_landkreis(tab):
+    return tab.select_one('th').string in ['Landkreis', 'Land-/Stadtkreis', 'Landkreis/Stadt']
+
+def is_confirmed(tab):
+    cap = tab.select_one('caption')
+    return cap is None or 'Coronavirusinfektionen' in cap.get_text()
+
+def is_deaths(tab):
+    cap = tab.select_one('caption')
+    return cap is not None and 'Todesfälle' in cap.get_text()
+
+rparse = fetchhelper.ParseData(update, 'regierungsbezirk')
+parse_table(rparse, html, select=(lambda tab: is_regierungsbezirk(tab) and is_confirmed(tab)))
+
+rdparse = fetchhelper.ParseData(update, 'regierungsbezirk_deaths')
+parse_table(rdparse, html, select=(lambda tab: is_regierungsbezirk(tab) and is_deaths(tab)), optional=True)
+
+lparse = fetchhelper.ParseData(update, 'landkreis')
+parse_table(lparse, html, select=(lambda tab: is_landkreis(tab) and is_confirmed(tab)))
+
+ldparse = fetchhelper.ParseData(update, 'landkreis_deaths')
+parse_table(ldparse, html, select=(lambda tab: is_landkreis(tab) and is_deaths(tab)), optional=True)
+
+fetchhelper.git_commit([rparse, rdparse, lparse, ldparse], args)
