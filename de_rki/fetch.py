bf53f0f6ac5ad262a44dc29a1a13ff233dc43aa0
Stephan Hohe <sth.dev@tejp.de>
2020-05-29 10:51:35 +0200

    Check area names

--- de_rki/fetch.py
+++ de_rki/fetch.py
@@ -33,8 +33,22 @@ if mo is None:
 update.contenttime = datetime.datetime.strptime(mo.group(1), '%d.%m.%Y, %H:%M') \
     .replace(tzinfo=datatz)
 
+aliases = {
+    'Branden-burg': 'Brandenburg',
+    'Meck-lenburg-Vorpommern': 'Mecklenburg-Vorpommern',
+    'Nord-rhein-Westfalen': 'Nordrhein-Westfalen',
+    'Nieder-sachsen': 'Niedersachsen',
+}
+
+good = set([
+    'Baden-Württemberg', 'Bayern', 'Berlin', 'Brandenburg', 'Bremen', 'Hamburg', 'Hessen',
+    'Mecklenburg-Vorpommern', 'Niedersachsen', 'Nordrhein-Westfalen', 'Rheinland-Pfalz',
+    'Saarland', 'Sachsen', 'Sachsen-Anhalt', 'Schleswig-Holstein', 'Thüringen',
+])
+
 def clean_label(lstr):
-    return lstr.replace('\n', '').replace('\xad', '').replace('*', '')
+    lstr = lstr.replace('\n', '').replace('\xad', '').replace('*', '')
+    return aliases.get(lstr, lstr)
 
 def clean_num(numstr):
     return int(numstr.replace('.', '').strip() or '0')
@@ -57,7 +71,9 @@ for tab in header.parent.parent.select('table'):
             tds = tr.select('td')
             assert(len(tds) in [5, 6])
             area = clean_label(tds[0].get_text())
-
+            if area not in good:
+                print("unknown area:", area, file=sys.stderr)
+                sys.exit(1)
             econfirmed = clean_num(tds[1].get_text())
             edeaths = clean_num(tds[5].get_text())
             cout.writerow([area, parse.parsedtime.isoformat(),
