2ebe4898d64f8d28020077673cdee2599e44a3a7
Stephan Hohe <sth.dev@tejp.de>
2020-09-08 23:18:38 +0200

    Add option for --compress

--- helper/fetchhelper.py
+++ helper/fetchhelper.py
@@ -19,7 +19,7 @@ class Updater(object):
         self.rawfile = self.rawname = self.rawtime = self.rawdata = None
         self.contenttime = None
 
-    def check_fetch(self, rawfile=None, *, binary=False, remotetime=False):
+    def check_fetch(self, rawfile=None, *, binary=False, remotetime=False, compressed=False):
         if rawfile is None:
             checkdir('raw')
             self.rawfile = 'raw/%s.%s' % (datetime.datetime.now().isoformat(), self.ext)
@@ -27,6 +27,8 @@ class Updater(object):
             opts = []
             if remotetime:
                 opts += ['-R']
+            if compressed:
+                opts += ['--compressed']
             subprocess.run(['curl', '-sS', '-L', '-o', self.rawfile] + opts + [self.fetchurl])
         else:
             self.rawfile = rawfile
