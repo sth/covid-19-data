1a1e8b53cbc2c695031b52b700562402f1319c7e
Stephan Hohe <sth.dev@tejp.de>
2020-09-16 13:24:17 +0200

    Adjust to minor site changes; Split out old fetch version t oldfetch

--- at_sgpk/fetch.py
+++ at_sgpk/fetch.py
@@ -10,6 +10,8 @@ ap = argparse.ArgumentParser()
 fetchhelper.add_arguments(ap)
 args = ap.parse_args()
 
+fetchhelper.check_oldfetch(args)
+
 import subprocess, datetime, re, csv, os
 from datetime import datetime
 from bs4 import BeautifulSoup
@@ -25,118 +27,61 @@ update.check_fetch(rawfile=args.rawfile)
 
 html = BeautifulSoup(update.rawdata, 'html.parser')
 
-class NotFound(Exception):
-    pass
-
-def parse_counts(parse, base, lead):
-    infotext = base.find("div", class_="infobox").find(string=re.compile(lead))
-    if infotext is None:
-        infotext = base.find("main", id="content").find(string=re.compile(lead))
-    txtp = infotext.find_parent('p')
-    if txtp is None:
-        raise NotFound()
-    txt = txtp.get_text()
-    mo = re.search(r'Stand (\d\d.\d\d.\d\d\d\d, \d\d:\d\d) Uhr', txt)
-    parse.parsedtime = datetime.strptime(mo.group(1), '%d.%m.%Y, %H:%M').replace(tzinfo=datatz)
-
-    txt = txt.replace('\xa0', ' ')
-    mo = re.search(r': ([0-9.]+)(?: Fälle)?, nach Bundesl.*ndern: (.*)\.\s*$', txt)
-    total = cleannum(mo.group(1))
-
-    with open(parse.parsedfile, 'w') as outf:
-        cout = csv.writer(outf)
-        cout.writerow(['Area', 'Date', 'Value'])
-        for mop in re.finditer(r'(.*?) \((.*?)\),?', mo.group(2)):
-            key = mop.group(1).strip()
-            value = cleannum(mop.group(2))
-            cout.writerow([key, parse.parsedtime.isoformat(), value])
-
-    parse.deploy_timestamp()
-
 def strip_footnote(s):
     return s.rstrip('*')
 
-def parse_v1(parses, html):
-    parse_c = fetchhelper.ParseData(update, 'confirmed')
-    parse_counts(parse_c, html, "Best.*tigte F.*lle")
-    parses.append(parse_c)
+table = fetchhelper.text_table(html.find('table'))
+ths = table[0]
+assert('Bundesland' in ths[0])
+assert('gesamt' in ths[-1])
+trs = table[1:]
+assert('tigte' in trs[0][0])
+assert('Todesf' in trs[1][0])
+assert('Genesen' in trs[2][0])
+assert('Hospital' in trs[3][0])
+assert('Intensiv' in trs[4][0])
+assert('Testungen' in trs[5][0])
+parse = [
+        fetchhelper.ParseData(update, 'confirmed'),
+        fetchhelper.ParseData(update, 'deaths'),
+        fetchhelper.ParseData(update, 'recovered'),
+        fetchhelper.ParseData(update, 'hospital'),
+        fetchhelper.ParseData(update, 'intensivecare'),
+        fetchhelper.ParseData(update, 'tests'),
+    ]
+labels = ['confirmed', 'deceased', 'recovered', 'hospital', 'intensivecare', 'tests']
 
-    # Seems to be removed for good
-    #parse_r = fetchhelper.ParseData(update, 'recovered')
-    #try:
-    #    parse_counts(parse_r, infobox, "Genesene Personen")
-    #    parses.append(parse_r)
-    #except AttributeError as err:
-    #    # It seems to be removed, we ignore it
-    #    print(err)
-
-    parse_d = fetchhelper.ParseData(update, 'deceased')
-    parse_counts(parse_d, html, "Todesf.*lle")
-    parses.append(parse_d)
-
-def parse_v2(parses, html):
-    table = fetchhelper.text_table(html.find('table'))
-    ths = table[0]
-    assert('Bundesland' in ths[0])
-    assert('gesamt' in ths[-1])
-    trs = table[1:]
-    assert('tigte' in trs[0][0])
-    assert('Todesf' in trs[1][0])
-    assert('Genesen' in trs[2][0])
-    assert('Hospital' in trs[3][0])
-    assert('Intensiv' in trs[4][0])
-    assert('Testungen' in trs[5][0])
-    parse = [
-            fetchhelper.ParseData(update, 'confirmed'),
-            fetchhelper.ParseData(update, 'deaths'),
-            fetchhelper.ParseData(update, 'recovered'),
-            fetchhelper.ParseData(update, 'hospital'),
-            fetchhelper.ParseData(update, 'intensivecare'),
-            fetchhelper.ParseData(update, 'tests'),
-        ]
-    labels = ['confirmed', 'deceased', 'recovered', 'hospital', 'intensivecare', 'tests']
-
-    areas = {
-            'Bgld.': 'Burgenland',
-            'Kt.': 'Kärnten',
-            'Ktn.': 'Kärnten',
-            'NÖ': 'Niederösterreich',
-            'OÖ': 'Oberösterreich',
-            'Sbg.': 'Salzburg',
-            'Stmk.': 'Steiermark',
-            'T': 'Tirol',
-            'Vbg.': 'Vorarlberg',
-            'W': 'Wien'
-        }
-
-    for i, tds in enumerate(trs):
-        assert(len(ths) == len(tds))
-        mo = re.search(r'Stand (\d\d.\d\d.\d\d\d\d), *(\d\d:\d\d) ?Uhr', tds[0])
-        if mo is None:
-            print("cannot parse date")
-            sys.exit(1)
-        parse = fetchhelper.ParseData(update, labels[i])
-        datadate = parse.parsedtime = datetime.strptime(mo.group(1) + ' ' + mo.group(2), '%d.%m.%Y %H:%M').replace(tzinfo=datatz)
-        with open(parse.parsedfile, 'w') as f:
-            cw = csv.writer(f)
-            cw.writerow(['Area', 'Date', 'Value'])
-            for col in range(1, len(tds)-1):
-                area = areas[strip_footnote(ths[col])]
-                count = cleannum(tds[col])
-                cw.writerow([area, datadate.isoformat(), count])
-        parse.deploy_timestamp()
-        parses.append(parse)
-
-def parse_all(parses, html):
-    try:
-        parse_v1(parses, html)
-        return
-    except NotFound:
-        pass
-
-    parse_v2(parses, html)
+areas = {
+        'Bgld.': 'Burgenland',
+        'Kt.': 'Kärnten',
+        'Ktn.': 'Kärnten',
+        'NÖ': 'Niederösterreich',
+        'OÖ': 'Oberösterreich',
+        'Sbg.': 'Salzburg',
+        'Stmk.': 'Steiermark',
+        'T': 'Tirol',
+        'Vbg.': 'Vorarlberg',
+        'W': 'Wien'
+    }
 
 parses = []
-parse_all(parses, html)
+
+for i, tds in enumerate(trs):
+    assert(len(ths) == len(tds))
+    mo = re.search(r'Stand (\d\d\.\d\d\.\d\d\d\d),[ \xa0]*(\d\d:\d\d) ?Uhr', tds[0])
+    if mo is None:
+        print("cannot parse date", file=sys.stderr)
+        sys.exit(1)
+    parse = fetchhelper.ParseData(update, labels[i])
+    datadate = parse.parsedtime = datetime.strptime(mo.group(1) + ' ' + mo.group(2), '%d.%m.%Y %H:%M').replace(tzinfo=datatz)
+    with open(parse.parsedfile, 'w') as f:
+        cw = csv.writer(f)
+        cw.writerow(['Area', 'Date', 'Value'])
+        for col in range(1, len(tds)-1):
+            area = areas[strip_footnote(ths[col])]
+            count = cleannum(tds[col])
+            cw.writerow([area, datadate.isoformat(), count])
+    parse.deploy_timestamp()
+    parses.append(parse)
 
 fetchhelper.git_commit(parses, args)
