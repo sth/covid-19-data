8280d8588213626de204440286da4097fe065a91
Stephan Hohe <sth.dev@tejp.de>
2021-04-28 05:05:02 +0200

    Parse new data for de_by_dah_lra

--- de_by_dah_lra/fetch.py
+++ de_by_dah_lra/fetch.py
@@ -12,36 +12,76 @@ args = ap.parse_args()
 
 fetchhelper.check_oldfetch(args)
 
+if args.rawfile is None:
+    rawfiles = (None, None)
+else:
+    rawfiles = args.rawfile.split(',')
+
 import datetime, re, csv, json, os, sys, shutil
 import dateutil.tz
 
-url = 'https://www.landratsamt-dachau.de/bilder/zahlen.jpg'
-url = 'https://atlas.jifo.co/api/connectors/c3a4b965-0e10-46db-aec6-cceffdb74857'
+url_cases = 'https://atlas.jifo.co/api/connectors/41be7d71-7260-497f-a60b-adce5aa9445d'
+url_recovered = 'https://atlas.jifo.co/api/connectors/2adaf217-e526-492a-bcad-5ed6ec6ad3ad'
 
 datatz = dateutil.tz.gettz('Europe/Berlin')
 
-update = fetchhelper.Updater(url, ext='json')
-update.check_fetch(rawfile=args.rawfile)
+update = fetchhelper.Updater(url_cases, ext='cases.json')
+update.check_fetch(rawfile=rawfiles[0])
 jdat = json.loads(update.rawdata)
 
 header = jdat['data'][0][0]
-i_kom = header.index("Gemeinde")
-i_con = header.index("Fälle insgesamt")
-i_rec = header.index("Genesen")
+i_kom = header.index("Ort")
+i_con = header.index("Gesamtzahl seit Ausbruch")
+
+parses = []
 
 parse = fetchhelper.ParseData(update, 'data')
 parse.parsedtime = datetime.datetime.fromtimestamp(jdat['refreshed']/1000, tz=datatz)
 with open(parse.parsedfile, 'w') as f:
     cw = csv.writer(f)
-    cw.writerow(['Kommune', 'Timestamp', 'Confirmed', 'Recovered'])
+    cw.writerow(['Kommune', 'Timestamp', 'Confirmed'])
     for jrow in jdat['data'][0][1:]:
+        if jrow[i_kom] in ('Zuordnung fehlt', 'Gesamt', ''):
+            continue
         if jrow[i_kom] == 'Pfaffenhofen a.d.Glonn':
             jrow[i_kom] = 'Pfaffenhofen a.d. Glonn'
-        cw.writerow([jrow[i_kom], parse.parsedtime.isoformat(), jrow[i_con], jrow[i_rec]])
+        cw.writerow([jrow[i_kom], parse.parsedtime.isoformat(), jrow[i_con]])
 
 parse.deploy_timestamp()
+
 if fetchhelper.csv_equal(parse.deployfile, parse.deployfile_previous(), skip=['Timestamp']):
     os.unlink(parse.deployfile)
 else:
-    fetchhelper.git_commit([parse], args)
+    parses.append(parse)
 
+
+update = fetchhelper.Updater(url_recovered, ext='rec.json')
+update.check_fetch(rawfile=rawfiles[1])
+jdat = json.loads(update.rawdata)
+
+header = jdat['data'][0][0]
+i_kom = header.index("Ort")
+i_rec = 1
+assert(header[i_rec].startswith("Stand "))
+
+parse = fetchhelper.ParseData(update, 'recovered')
+parse.parsedtime = datetime.datetime.fromtimestamp(jdat['refreshed']/1000, tz=datatz)
+with open(parse.parsedfile, 'w') as f:
+    cw = csv.writer(f)
+    cw.writerow(['Kommune', 'Timestamp', 'Recovered'])
+    for jrow in jdat['data'][0][1:]:
+        if jrow[i_kom] in ('Zuordnung fehlt', 'Gesamt', ''):
+            continue
+        if jrow[i_kom] == 'Pfaffenhofen a.d.Glonn':
+            jrow[i_kom] = 'Pfaffenhofen a.d. Glonn'
+        cw.writerow([jrow[i_kom], parse.parsedtime.isoformat(), jrow[i_rec]])
+
+parse.deploy_timestamp()
+
+if fetchhelper.csv_equal(parse.deployfile, parse.deployfile_previous(), skip=['Timestamp']):
+    os.unlink(parse.deployfile)
+else:
+    parses.append(parse)
+
+if parses:
+    fetchhelper.git_commit(parses, args)
